2490f95856d7a13c3c6ae6ef410d87ab3665b599
Deny <den687@mail.ru>
2022-06-28 17:29:55 +0300

    Improved 'C' task

--- C/C.py
+++ C/C.py
@@ -1,26 +1,26 @@
-import sys
 import math
+import sys
+import time
+import timeit
 
 
-def get_prime_numbers():
-    prime_numbers = [2]
-    for i in range(3, 1000000, 2):
-        if (i > 10) and (i % 10 == 5):
-            continue
-        for j in prime_numbers:
-            if j*j - 1 > i:
-                prime_numbers.append(i)
-                break
-            if i % j == 0:
-                break
-        else:
-            prime_numbers.append(i)
-    return prime_numbers
+def get_prime_numbers(limit=sys.maxsize // 10**12):
+    if limit < 2:
+        return []
+
+    limit += 1  # Preincrement `limit` so sieve is inclusive, unlike `range`.
+    primes = [True]*limit
+    for base in range(2, int(limit**0.5 + 1)):
+        if primes[base]:
+            primes[base*2:limit:base] = [False]*(math.ceil(limit / base) - 2)
+
+    primes[0] = primes[1] = False
+    return (num for num, is_prime in enumerate(primes) if is_prime)
 
 
-def get_max_gcd(A, B):
+def get_max_gcd(A, B, prime_numbers):
     max_gcd = math.gcd(A, B)
-    for prime_number in get_prime_numbers():
+    for prime_number in prime_numbers:
         current_gcd_A = math.gcd(A * prime_number, B)
         current_gcd_B = math.gcd(A, B * prime_number)
         if current_gcd_A > max_gcd:
@@ -31,15 +31,25 @@ def get_max_gcd(A, B):
 
 
 def main():
-    T = int(sys.stdin.readline())
     tasks = []
+    prime_numbers = get_prime_numbers()
 
-    for _ in range(T):
-        tasks.append(tuple(map(int, sys.stdin.readline().split())))
+    with open("C/input.txt") as f:
+        T = int(f.readline())
+        for _ in range(T):
+            tasks.append(tuple(map(int, f.readline().split())))
 
-    for task in tasks:
-        print(get_max_gcd(task[0], task[1]))
+    with open("C/output.txt", "w") as f:
+        for task in tasks:
+            f.write("{0} ".format(get_max_gcd(
+                task[0], task[1], prime_numbers)))
 
 
 if __name__ == "__main__":
-    main()
+    # start_time = time.time()
+    # main()
+    # elapsed_time = time.time() - start_time
+    # print("Execution time: {:.3f} seconds".format(elapsed_time))
+
+    elapsed_time = timeit.timeit(stmt='main()', globals=globals(), number=1)
+    print("Execution time: {:.3f} seconds".format(elapsed_time))
